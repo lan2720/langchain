92539680e144f98f2c6da87763ecc1d5610f4dd9
jarvixwang <jarvixwang@futunn.com>
2023-04-19 05:13:54 +0800

    test

--- my_demo/try_memory.py
+++ my_demo/try_memory.py
@@ -5,7 +5,7 @@ from langchain.prompts import PromptTemplate
 from config import OPENAI_API_KEY
 
 llm = OpenAI(openai_api_key=OPENAI_API_KEY, temperature=0)
-print()
+
 # llm = OpenAI(temperature=0) # Can be any valid LLM
 _DEFAULT_TEMPLATE = """The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
 
